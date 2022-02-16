0522c1c0f29cd8d949095817f4d4bc248a83dde4
MariiaHavlo <mariakolodiy01@gmail.com>
2022-02-17 01:41:58 +0200

    classwork1

--- homework1/app.js
+++ homework1/app.js
@@ -6,20 +6,20 @@
 const fs = require('fs')
 const path = require('path')
 
-fs.writeFile(path.join(__dirname, 'test.txt'),'some data',(err)=>{
-    if (err){
+fs.writeFile(path.join(__dirname, 'test.txt'), 'some data', (err) => {
+    if (err) {
         console.log(err);
         throw err
     }
 })
-fs.readFile(path.join(__dirname,'test.txt'),'utf8',(err,data)=>{
-    if (err){
+fs.readFile(path.join(__dirname, 'test.txt'), 'utf8', (err, data) => {
+    if (err) {
         console.log(err)
-            throw err
+        throw err
     }
     console.log(data)
-    fs.writeFile(path.join(__dirname,'test2.txt'),`${data}`,(err)=>{
-        if (err){
+    fs.writeFile(path.join(__dirname, 'test2.txt'), `${data}`, (err) => {
+        if (err) {
             console.log(err)
             throw err
         }
@@ -31,37 +31,83 @@ fs.readFile(path.join(__dirname,'test.txt'),'utf8',(err,data)=>{
 // Прочитайте його, скопіюйте всі дані з нього і перенесіть їх в нову папку та файл в ній, старий файл видаліть після
 // того як все завершиться. Також вийде callback hell
 
-fs.writeFile(path.join(__dirname,'task2.txt'),'info about task',(err)=>{
-    if (err){
+fs.writeFile(path.join(__dirname, 'task2.txt'), 'info about task', (err) => {
+    if (err) {
         console.log(err)
         throw err
     }
 })
 
-fs.readFile(path.join(__dirname,'task2.txt'),'utf8',(err,data)=>{
-    if (err){
+fs.readFile(path.join(__dirname, 'task2.txt'), 'utf8', (err, data) => {
+    if (err) {
         console.log(err)
         throw err
     }
     console.log(data)
 
-    fs.mkdir(path.join(__dirname,'tasks'),(err)=>{
-        if (err){
+    fs.mkdir(path.join(__dirname, 'tasks'), (err) => {
+        if (err) {
             console.log(err)
             throw err
         }
 
-        fs.wriiteFile(path.join(__dirname,'taskMain.txt'),`${data}`,(err)=>{
-            if (err){
+        fs.wriiteFile(path.join(__dirname, 'taskMain.txt'), data.toString(), (err) => {
+            if (err) {
                 console.log(err)
                 throw err
 
             }
+            fs.unlink(path.join(__dirname, 'taskMain.txt'), (err) => {
+                if (err) {
+                    console.log(err)
+                    throw err
+
+                }
+            })
         })
     })
 
 })
 //
-// 3. Створіть папку (можете вручну) напишіть скріпт який створить в ній якись дані (можуть бути нові папки і файли(в файли запишіть якусь дату) )
-// і напишіть функцію яка буде зчитувати папку і перевіряти якщо дані які в ній лежать - це файли тоді вам потрібно їх очистити, але не видаляти, якщо дані - це папки, вам потрібно їх перейменувати і додати до назви префікс _new
+// 3. Створіть папку (можете вручну) напишіть скріпт який створить в ній якись дані (можуть бути нові папки і файли
+// (в файли запишіть якусь дату) )
+// і напишіть функцію яка буде зчитувати папку і перевіряти якщо дані які в ній лежать - це файли тоді вам потрібно
+// їх очистити, але не видаляти, якщо дані - це папки, вам потрібно їх перейменувати і додати до назви префікс _new
+
+fs.mkdir(path.join(__dirname, 'task3', 'main', 'about'), {recursive: true}, (err) => {
+    if (err) {
+        console.log(err)
+        throw err
+    }
+})
+fs.writeFile(path.join(__dirname, 'main', 'about', 'info.txt'), 'info about user', (err) => {
+    if (err) {
+        console.log(err)
+        throw err
+    }
+})
+fs.readdir(path.join(__dirname, 'task3'), (err, data) => {
+    if (err) {
+        console.log(err)
+        throw err
+    }
+    data.map(value => {
+        const valueEnd = value.endsWith('txt')
+        if (valueEnd === true) {
+            fs.truncate(path.join(__dirname, 'task3', value), (err) => {
+                if (err) {
+                    console.log(err);
+                    throw err
+                }
+            })
+        } else {
+            fs.rename(path.join(__dirname, 'task', 'new' + value), (err) => {
+                if (err) {
+                    console.log(err);
+                    throw err
+                }
+            })
+        }
+    })
+})
 
